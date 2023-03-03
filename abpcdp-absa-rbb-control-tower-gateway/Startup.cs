e66ed9e135b7e5be95225206942ff92ea301b8ed
mahappys <98953654+mahappys@users.noreply.github.com>
2023-03-03 14:59:01 +0200

    first checkin

--- abpcdp-absa-rbb-control-tower-gateway/Startup.cs
+++ abpcdp-absa-rbb-control-tower-gateway/Startup.cs
@@ -77,8 +77,7 @@ namespace abpcdp_absa_rbb_control_tower_gateway
             });
             services.AddOcelot().AddPolly();
         }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+                // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
